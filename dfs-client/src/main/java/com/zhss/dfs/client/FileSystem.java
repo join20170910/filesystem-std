033388f1faf92259dab46d3da224335135f2b872
apple <17717990@qq.com>
2023-09-01 20:15:23 +0800

    dfs mkdir

---
+++ dfs-client/src/main/java/com/zhss/dfs/client/FileSystem.java
@@ -0,0 +1,8 @@
+package com.zhss.dfs.client;
+
+/**
+ * 作为文件系统的接口
+ */
+public interface FileSystem {
+    void mkdir(String path) throws Exception;
+}
---
+++ dfs-client/src/main/java/com/zhss/dfs/client/FileSystemImpl.java
@@ -0,0 +1,14 @@
+package com.zhss.dfs.client;
+
+/**
+ * 文件系统客户端的实现类
+ */
+public class FileSystemImpl implements FileSystem {
+    /**
+     * 目录创建
+     * @param path
+     * @throws Exception
+     */
+    @Override
+  public void mkdir(String path) throws Exception {}
+}
